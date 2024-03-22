a3e700b0e2989a06729239f2842e6e3459853bc1
Prazol Malla <prajwalmalla16@gmail.com>
2024-03-22 12:58:32 +0545

    responsive till tablet and small laptop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
-const header = document.getElementById("header")
+
+// **** BLUR EFFECT ON SCROLL ****
+const header = document.getElementById("header");
 
 const navscroll = ()=>{
     if(this.scrollY>50){
-        header.classList.add('blur-header')
+        header.classList.add('blur-header');
     }
     else{
-        header.classList.remove('blur-header')
+        header.classList.remove('blur-header');
     }
     
 }
 
-window.addEventListener('scroll',navscroll)
+window.addEventListener('scroll',navscroll);
+
+const hamMenu = document.querySelector('.nav-ham');
+const navMenu = document.querySelector('.nav-menu') 
+const cross = document.querySelector('.cross')
+
+hamMenu.addEventListener('click',function(){
+    navMenu.style.right="0";
+    // navMenu.style.transition ="right 0.4s";
+});
+
+cross.addEventListener('click',function(){
+    navMenu.style.right="-1000px";
+});
